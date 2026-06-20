109ff3dc244427fd957ca783a60c8103feaf06bc
codesense-ai-two <pazpes@mail.fastbolt.vip>
2026-06-20 22:59:38 +0530

    ✅ Minimum Platforms | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-06-20

---
+++ gfg/medium/minimum-platforms-1587115620/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minPlatform(vector<int>& arr, vector<int>& dep) {
+        int n = arr.size();
+
+        sort(arr.begin(), arr.end());
+        sort(dep.begin(), dep.end());
+
+        int i = 0, j = 0;
+        int platforms = 0, ans = 0;
+
+        while (i < n && j < n) {
+            if (arr[i] <= dep[j]) {
+                platforms++;
+                ans = max(ans, platforms);
+                i++;
+            } else {
+                platforms--;
+                j++;
+            }
+        }
+
+        return ans;
+    }
+};
